a07e3a2b45e460b47cf10dad3dc53c2534102ea7
danweaver01 <danweaver01@icloud.com>
2022-10-12 15:27:06 -0400

    added bucket acl

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "public-bucket-oops" {
   bucket = "my-public-bucket-oops"
+  acl    = "private"
   tags = {
     yor_trace = "40a0c807-97a2-4370-a0ad-565f8e4b59e3"
   }
